8630d53b9a51149414b6dab687e9005ea69a9417
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-09-22 16:36:33 -0400

    update server addr

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import environ
 @environ.config
 class Config:
     target_domain = environ.var('https://osugidani.jp')
-    local_domain = environ.var('http://localhost:5000')
+    local_domain = environ.var('https://i.sg.ndxk.cc')
     port = environ.var(5000, converter=int)
     skip_ascii = environ.bool_var(True)
 
